3a24c68b1f0be476df1c270bad71f6fc4fba3e74
刘白菜 <liushuai.baicai@hotmail.com>
2017-05-10 18:43:08 +0800

    update reader

--- application.rb
+++ application.rb
@@ -55,44 +55,13 @@ end
 get '/:book_id/:catalog_id.html' do
   @catalog = Catalog.first(:book_id => params['book_id'].to_i , :catalog_id => params['catalog_id'].to_i)
 
-  doc = Nokogiri::HTML(open(@catalog.src), nil, 'UTF-8')
-  n = doc.css('div#content')[0]
-  n.search('script').remove
-  @content = n.inner_html.gsub!('<br>　　<br>', '<br>')
+  # doc = Nokogiri::HTML(open(@catalog.src), nil, 'UTF-8')
+  # n = doc.css('div#content')[0]
+  # n.search('script').remove
+  # @content = n.inner_html.gsub!('<br>　　<br>', '<br>')
 
   @next_catalog = Catalog.first(:book_id => params['book_id'].to_i , :catalog_id.gt => params['catalog_id'].to_i)
   @prev_catalog = Catalog.last(:book_id => params['book_id'].to_i , :catalog_id.lt => params['catalog_id'].to_i)
 
   erb :detail
 end
-
-
-# <script>
-#   var target_src = '<%= @catalog.src %>';
-#
-#   $(function(){
-#
-#   $.ajaxPrefilter( function (options) {
-#     if (options.crossDomain && jQuery.support.cors) {
-#         var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-#     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-#     //options.url = "http://cors.corsproxy.io/url=" + options.url;
-#     }
-#     });
-#     $.ajax({
-#                async: false,
-#                type: "get",
-#                url: target_src,
-#                data: "",
-#                timeout: 3000,
-#                contentType: "text/html;charset=utf-8",
-#                beforeSend: function (xhr) {
-#                  xhr.setRequestHeader('X-Requested-With', 'x-requested-with');
-#                },
-#                success: function(data) {
-#                  $("#book-content").html($(data).find("div#content").html().replace(/<br>　　<br>/g, '<br>'));
-#                }
-#            });
-#
-#     })
-# </script>
