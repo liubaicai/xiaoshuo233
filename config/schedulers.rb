3777804b69e957da87401ad2ca884c306ce240ad
刘白菜 <liushuai.baicai@hotmail.com>
2017-05-24 12:17:30 +0800

    update script

--- config/schedulers.rb
+++ config/schedulers.rb
@@ -6,7 +6,7 @@ scheduler.every '1m' do
   begin
 
     unless File.exist?("#{File.dirname(__FILE__)}/../script/dushu233.pid")
-      `ruby #{File.dirname(__FILE__)}/../script/dushu233.rb`
+      `ruby #{File.dirname(__FILE__)}/../script/dushu233.rb &`
     end
 
   end
