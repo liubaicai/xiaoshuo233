2a1a22ca927aba6014181b016d73d98cd560f762
liubaicai <liushuai.baicai@hotmail.com>
2017-12-22 17:04:04 +0800

    多个分类查询

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,45 @@ var models = require('../models/models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    models.book.findOne({include: [models.category]}).then(function (book) {
-        res.render('index', {
-            title: book.category.title,
-        });
+    var promises = new Array()
+    var config = {}
+
+    var recommend = models.book.findAll({
+        order: [ [ 'views', 'DESC' ] ],
+        include: [models.category],
+        limit: 6
+    }).then(function (books) {
+        config.books_recommend = books
     });
+
+    var hot = models.book.findAll({
+        order: [ [ 'views', 'DESC' ] ],
+        include: [models.category],
+        limit: 10
+    }).then(function (books) {
+        config.books_hot = books
+    });
+
+    var cates = models.category.findAll({
+        include: [{
+            model: models.book,
+            order: [ [ 'views', 'DESC' ] ],
+            limit: 10
+        }]}).then(function (categories) {
+        config.categories = categories
+    });
+    promises.push(recommend);
+    promises.push(hot);
+    promises.push(cates);
+    Promise.all(promises)
+        .then(function(results){
+            console.log(results)
+            res.render('index', {
+                books_recommend: config.books_recommend,
+                books_hot: config.books_hot,
+                categories: config.categories,
+            });
+        });
 });
 
 module.exports = router;
